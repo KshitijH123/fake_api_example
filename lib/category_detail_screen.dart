f45849267e028e92ad469c05fa654b9bf6780e29
KshitijH123 <kshitijhapase@gmail.com>
2025-02-12 22:35:29 +0530

    category screen

---
+++ lib/category_detail_screen.dart
@@ -0,0 +1,60 @@
+import 'package:fake_api_example/model_class.dart/get_api_categories_controller.dart';
+import 'package:fake_api_example/model_class.dart/get_api_categories_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CategoryDetailScreen extends StatelessWidget {
+  final CategoryDetailController _categoryDetailController =
+      Get.put(CategoryDetailController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Category Details'),
+      ),
+      body: GetBuilder<CategoryDetailController>(
+        init: _categoryDetailController,
+        builder: (controller) {
+          return controller.categories.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: controller.categories.length,
+                  itemBuilder: (context, index) {
+                    CategoryDetailModel category = controller.categories[index];
+                    return Card(
+                      elevation: 4.0,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(16.0),
+                        title: Text(
+                          category.name,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        subtitle: Text(
+                          category.slug,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        trailing: const Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.blueAccent,
+                        ),
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('URL: ${category.url}'),
+                              backgroundColor: Color.fromARGB(255, 46, 208, 24),
+                              duration: const Duration(seconds: 3),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+        },
+      ),
+    );
+  }
+}
